b4ac2a06172341af56d188f0aa3c0633604fa547
Benjamin Bonneau <27144402+857b@users.noreply.github.com>
2023-06-18 21:09:51 +0200

    tests

--- Test/VProg1.v
+++ Test/VProg1.v
@@ -32,8 +32,7 @@ Ltac init_Test :=
   unshelve eexists; [ Tac.build_FSpec | Tac.erase_impl | cbn ].
 
 
-Definition test_Ret :
-  Test SPEC (p : ptr)
+Goal Test SPEC (p : ptr)
   'n [vptr p ~> n] [] True
   '(r : nat) tt [] True
 (fun _ =>
@@ -43,8 +42,9 @@ Proof.
   solve_by_wlp.
 Qed.
 
-Definition test_dlet :
-  Test SPEC ((ps, p1) : (ptr * ptr) * ptr)
+Section DLet. (* destructive let *)
+
+Goal Test SPEC ((ps, p1) : (ptr * ptr) * ptr)
   '(n0, n1) [vptr (fst ps) ~> n0] [vptr p1 ~> n1] True
   '(_ : unit) tt [vptr p1 ~> O] True
 (fun '(ps, p1) =>
@@ -56,181 +56,144 @@ Proof.
   FunProg.solve_by_wlp.
 Qed.
 
-
-(* destructive let *)
-
-Definition spec_a0a : FSpec _
-  SPEC (ps : ptr * ptr) 'n [vptr (fst ps) ~> n] [] True
-  '(_ : unit) tt [] True.
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a0a : m_body spec_a0a := fun ps =>
+Goal Test SPEC (ps : ptr * ptr)
+  'n [vptr (fst ps) ~> n] [] True
+  '(_ : unit) tt [] True
+(fun ps =>
   'v0 <- let (p0, _) := ps in Read p0;
-  Ret tt.
-Goal f_body_match vprog_a0a (m_spec spec_a0a).
-Proof. solve_by_wlp. Qed.
-Goal f_erase vprog_a0a.
-Proof. Tac.erase_impl. Defined.
+  Ret tt).
+Proof.
+  init_Test.
+  build_fun_spec.
+  FunProg.solve_by_wlp.
+Qed.
 
-
-Definition spec_a0b : FSpec _
-  SPEC (ps : ptr * ptr) 'n [vptr (fst ps) ~> n] [] True
-  '(_ : memdata) tt [] True.
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a0b : m_body spec_a0b := fun ps =>
+Goal Test SPEC (ps : ptr * ptr)
+  'n [vptr (fst ps) ~> n] [] True
+  '(_ : memdata) tt [] True
+(fun ps =>
   let (p0, _) := ps in
-  Read p0.
-Goal f_body_match vprog_a0b (m_spec spec_a0b).
-Proof. solve_by_wlp. Qed.
-Goal f_erase vprog_a0b.
-Proof. Tac.erase_impl. Defined.
+  Read p0).
+Proof.
+  init_Test.
+  build_fun_spec.
+  FunProg.solve_by_wlp.
+Qed.
 
-
-Definition spec_a0c : FSpec _
-  SPEC (ns : nat * nat) '(v0, v1) [] [vdummy (Fin.t (fst ns)) ~> v0; vdummy (Fin.t (snd ns)) ~> v1] True
-  '(_ : unit) tt [] True.
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a0c : m_body spec_a0c := fun ns =>
+Goal Test SPEC (ns : nat * nat)
+  '(v0, v1) [] [vdummy (Fin.t (fst ns)) ~> v0; vdummy (Fin.t (snd ns)) ~> v1] True
+  '(_ : unit) tt [] True
+(fun ns =>
   let (n0, n1) := ns in
   gLem remove_vdummy (Fin.t n0);;
-  gLem remove_vdummy (Fin.t n1).
-Goal f_body_match vprog_a0c (m_spec spec_a0c).
+  gLem remove_vdummy (Fin.t n1)).
 Proof.
+  init_Test.
   build_fun_spec.
   FunProg.solve_by_wlp.
 Qed.
-Goal f_erase vprog_a0c.
-Proof. Tac.erase_impl. Defined.
 
-
-Definition spec_a0d : FSpec _
-  SPEC (ns : nat * nat) & (_ : Fin.t (fst ns)) 'v [] [vdummy (Fin.t (snd ns)) ~> v] True
-  '(r : nat) & (_ : Fin.t r) tt [] True.
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a0d : m_body spec_a0d := fun ns g =>
+Goal Test SPEC (ns : nat * nat) & (_ : Fin.t (fst ns))
+  'v [] [vdummy (Fin.t (snd ns)) ~> v] True
+  '(r : nat) & (_ : Fin.t r) tt [] True
+(fun ns g =>
   (let (n0, n1) as ns return Fin.t (fst ns) -> _ := ns in fun g =>
    gLem remove_vdummy (Fin.t n1);;
-   RetG n0 g) g.
-Goal f_body_match vprog_a0d (m_spec spec_a0d).
+   RetG n0 g) g).
 Proof.
+  init_Test.
   build_fun_spec.
   FunProg.solve_by_wlp.
 Qed.
-Goal f_erase vprog_a0d.
-Proof. Tac.erase_impl. Defined.
 
-
-Definition spec_a0e : FSpec _
-  SPEC (ns : nat * nat) & (_ : Fin.t (fst ns) * Fin.t (snd ns)) 'tt [] [] True
-  '(_ : nat) tt [] True.
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a0e : m_body spec_a0e := fun ns gs =>
+Goal Test SPEC (ns : nat * nat) & (_ : Fin.t (fst ns) * Fin.t (snd ns))
+  'tt [] [] True
+  '(_ : nat) tt [] True
+(fun ns gs =>
   let (g0, g1) := gs in
   (let (n0, n1) as ns return Fin.t (fst ns) -> Fin.t (snd ns) -> _ := ns in fun g0 g1 =>
-   Ret n0) g0 g1.
-Goal f_body_match vprog_a0e (m_spec spec_a0e).
+   Ret n0) g0 g1).
 Proof.
+  init_Test.
   build_fun_spec.
   FunProg.solve_by_wlp.
 Qed.
-Goal f_erase vprog_a0e.
-Proof. Tac.erase_impl. Defined.
 
-(* branch *)
+End DLet.
 
-Definition spec_a1a : FSpec _
-  SPEC ((b, p0, p1, p2) : bool * ptr * ptr * ptr)
+Section Branch.
+
+Goal Test SPEC ((b, p0, p1, p2) : bool * ptr * ptr * ptr)
   '(n0, n1, n2) [vptr p0 ~> n0] [vptr p1 ~> n1; vptr p2 ~> n2] True
-  '(_ : unit) (n1', n2') [vptr p1 ~> n1'; vptr p2 ~> n2'] (b = true -> n1' = 0).
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a1a : m_body spec_a1a := fun '(b, p0, p1, p2) =>
+  '(_ : unit) (n1', n2') [vptr p1 ~> n1'; vptr p2 ~> n2'] (b = true -> n1' = 0)
+(fun '(b, p0, p1, p2) =>
   if b
   then Write p1 0
-  else Write p2 1.
-Goal f_body_match vprog_a1a (m_spec spec_a1a).
+  else Write p2 1).
 Proof.
+  init_Test.
   build_fun_spec.
   FunProg.solve_by_wlp; congruence.
 Qed.
-Goal f_erase vprog_a1a.
-Proof. Tac.erase_impl. Defined.
-
 
 Inductive bool3 : Set := B0 | B1 | B2.
 
-Definition spec_a1b : FSpec _
-  SPEC ((b, p) : bool3 * ptr)
+Goal Test SPEC ((b, p) : bool3 * ptr)
   'n [] [vptr p ~> n] (b <> B2)
-  '(_ : unit) n' [vptr p ~> n'] (b <> B1 -> n' = 0).
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a1b : m_body spec_a1b := fun '(b, p) =>
+  '(_ : unit) n' [vptr p ~> n'] (b <> B1 -> n' = 0)
+(fun '(b, p) =>
   match b with
   | B0 => Write p 0
   | B1 | B2 => Ret tt
-  end.
-Goal f_body_match vprog_a1b (m_spec spec_a1b).
-Proof. solve_by_wlp; congruence. Qed.
-Goal f_erase vprog_a1b.
-Proof. Tac.erase_impl. Defined.
+  end).
+Proof.
+  init_Test.
+  build_fun_spec.
+  FunProg.solve_by_wlp; congruence.
+Qed.
 
-
-Definition spec_a1c : FSpec _
-  SPEC ((o, p) : option nat * ptr)
+Goal Test SPEC ((o, p) : option nat * ptr)
   'n [] [vptr p ~> n] (n > 0 /\ match o with Some n' => n' > 0 | None => True end)
-  '(_ : unit) n' [vptr p ~> n'] (n' > 0).
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a1c : m_body spec_a1c := fun '(o, p) =>
+  '(_ : unit) n' [vptr p ~> n'] (n' > 0)
+(fun '(o, p) =>
   match o with
   | Some n => Write p n
   | None   => Ret tt
-  end.
-Goal f_body_match vprog_a1c (m_spec spec_a1c).
-Proof. solve_by_wlp. Qed.
-Goal f_erase vprog_a1c.
-Proof. Tac.erase_impl. Defined.
-
-
-Definition spec_a1d : FSpec _
-  SPEC (b : bool)
-  'v [] [(if b as b return Vprop.p (if b then nat else bool) then vdummy nat else vdummy bool) ~> v] True
-  '(_ : unit) tt [] True.
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a1d : m_body spec_a1d := fun b =>
-  if b
-  then gLem remove_vdummy nat
-  else gLem remove_vdummy bool.
-Goal f_body_match vprog_a1d (m_spec spec_a1d).
-Proof. solve_by_wlp. Qed.
-Goal f_erase vprog_a1d.
-Proof. Tac.erase_impl. Defined.
-
-
-Definition spec_a1e : FSpec _
-  SPEC (b : bool) & (_ : if b then nat else unit)
-  'tt [] [] True
-  '(_ : unit) & (_ : option nat) tt [] True.
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a1e : m_body spec_a1e := fun b g =>
-  (if b as b return (if b then nat else unit) -> _
-   then fun g => RetG tt (Some g)
-   else fun _ => RetG tt None
-  ) g.
-Goal f_body_match vprog_a1e (m_spec spec_a1e).
+  end).
 Proof.
+  init_Test.
   build_fun_spec.
   FunProg.solve_by_wlp.
 Qed.
-Goal f_erase vprog_a1e.
-Proof. Tac.erase_impl. Defined.
+
+Goal Test SPEC (b : bool)
+  'v [] [(if b as b return Vprop.p (if b then nat else bool) then vdummy nat else vdummy bool) ~> v] True
+  '(_ : unit) tt [] True
+(fun b =>
+  if b
+  then gLem remove_vdummy nat
+  else gLem remove_vdummy bool).
+Proof.
+  init_Test.
+  build_fun_spec.
+  FunProg.solve_by_wlp.
+Qed.
+
+Goal Test SPEC (b : bool) & (_ : if b then nat else unit)
+  'tt [] [] True
+  '(_ : unit) & (_ : option nat) tt [] True
+(fun b g =>
+  (if b as b return (if b then nat else unit) -> _
+   then fun g => RetG tt (Some g)
+   else fun _ => RetG tt None
+  ) g).
+Proof.
+  init_Test.
+  build_fun_spec.
+  FunProg.solve_by_wlp.
+Qed.
+
+End Branch.
 
 
 Definition spec_a2a : FSpec _
@@ -262,76 +225,62 @@ Goal f_erase vprog_a2b.
 Proof. Tac.erase_impl. Defined.
 
 
-Definition spec_a3a : FSpec _
-  SPEC (_ : unit) & ((n0, n1) : nat * nat)
+Goal Test SPEC (_ : unit) & ((n0, n1) : nat * nat)
   'tt [] [] True
-  '(_ : unit) & (r : nat) tt [] (r = n0 + n1).
-Proof. Tac.build_FSpec. Defined.
+  '(_ : unit) & (r : nat) tt [] (r = n0 + n1)
+(fun _ '(n0, n1) =>
+  RetG tt (n0 + n1)).
+Proof.
+  init_Test.
+  build_fun_spec.
+  FunProg.solve_by_wlp.
+Qed.
 
-Definition vprog_a3a : m_body spec_a3a := fun _ '(n0, n1) =>
-  RetG tt (n0 + n1).
-Goal f_body_match vprog_a3a (m_spec spec_a3a).
-Proof. solve_by_wlp. Qed.
-Goal f_erase vprog_a3a.
-Proof. Tac.erase_impl. Defined.
-
-
-Definition spec_a3b : FSpec _
-  SPEC ((_, _) : nat * unit) & (_ : nat)
+Goal Test SPEC ((_, _) : nat * unit) & (_ : nat)
   'tt [] [] True
-  '(_ : nat) tt [] True.
-Proof. Tac.build_FSpec. Defined.
+  '(_ : nat) tt [] True
+(fun '(n, _) _ =>
+  Ret n).
+Proof.
+  init_Test.
+  build_fun_spec.
+  FunProg.solve_by_wlp.
+Qed.
 
-Definition vprog_a3b : m_body spec_a3b := fun '(n, _) _ =>
-  Ret n.
-Goal f_body_match vprog_a3b (m_spec spec_a3b).
-Proof. solve_by_wlp. Qed.
-Goal f_erase vprog_a3b.
-Proof. Tac.erase_impl. Defined.
 
-
-Definition spec_a4a : FSpec _
-  SPEC ((p0, p1, p2) : ptr * ptr * ptr)
+Goal Test SPEC ((p0, p1, p2) : ptr * ptr * ptr)
   '(v0, v1) [vptr p2 ~> v1] [vptr p0 ~> v0] (vptr p0 = vptr p1)
-  '(_ : unit) tt [vptr p1 ~> v0] True.
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a4a : m_body spec_a4a := fun '(p0, p1, p2) =>
-  gRewrite (vptr p0) (vptr p1).
-Goal f_body_match vprog_a4a (m_spec spec_a4a).
+  '(_ : unit) tt [vptr p1 ~> v0] True
+(fun '(p0, p1, p2) =>
+  gRewrite (vptr p0) (vptr p1)).
 Proof.
+  init_Test.
   build_fun_spec.
   FunProg.solve_by_wlp.
 Qed.
-Goal f_erase vprog_a3b.
-Proof. Tac.erase_impl. Defined.
 
-
-Definition spec_a4b : FSpec _
-  SPEC ((n0, n1) : nat * nat)
+Goal Test SPEC ((n0, n1) : nat * nat)
   'v0 [] [vdummy (Fin.t n0) ~> v0] (n0 = n1)
-  '(_ : unit) v1 [vdummy (Fin.t n1) ~> v1] True.
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a4b : m_body spec_a4b := fun '(n0, n1) =>
-  gRewrite n0 n1.
-Goal f_body_match vprog_a4b (m_spec spec_a4b).
+  '(_ : unit) v1 [vdummy (Fin.t n1) ~> v1] True
+(fun '(n0, n1) =>
+  gRewrite n0 n1).
 Proof.
+  init_Test.
   build_fun_spec.
   FunProg.solve_by_wlp.
 Qed.
 
 
-Definition spec_a5a : FSpec _
-  SPEC (p : ptr)
+Goal Test SPEC (p : ptr)
   'v [] [vptr p ~> v] True
-  '(p' : ptr) tt [vptr p' ~> v] True.
-Proof. Tac.build_FSpec. Defined.
-
-Definition vprog_a5a : m_body spec_a5a := fun p =>
+  '(p' : ptr) tt [vptr p' ~> v] True
+(fun p =>
   't <- Ret p;
-  Ret p.
-Goal f_body_match vprog_a5a (m_spec spec_a5a).
-Proof. solve_by_wlp. Qed.
+  Ret p).
+Proof.
+  init_Test.
+  build_fun_spec.
+  FunProg.solve_by_wlp.
+Qed.
 
 End Test.
